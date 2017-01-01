8cdbbf319c17f077026b2c334d8aacc475b8afcd
Johan Kämpe <kampe.johan@gmail.com>
2017-01-01 09:34:10 +0100

    Added event loop, and ability to close window with esc key

--- main.c
+++ main.c
@@ -25,21 +25,49 @@ int main(int argc, char* args[]){
 					
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	
-	//RGB + ALPHA
-	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+	int done = 0;
+	SDL_Event event;
 	
-	SDL_RenderClear(renderer);
-	
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-	
-	SDL_Rect rect = { 220, 140, 200, 200 };
-	SDL_RenderFillRect(renderer, &rect);
-	
-	SDL_RenderPresent(renderer);
-	
-	SDL_Delay(10000);
-	
+	while(!done){
+		while(SDL_PollEvent(&event)){ //SDL_PollEvent returns true if an event shall be processed
+			switch(event.type){
+				case SDL_WINDOWEVENT_CLOSE: /**< The window manager requests that the window be closed */
+					if(window){
+						SDL_DestroyWindow(window);
+						window = NULL;
+					}
+					break;
+				case SDL_KEYDOWN:
+					switch(event.key.keysym.sym){
+						case SDLK_ESCAPE:
+							done = 1;
+							break;
+					}
+					break;
+				case SDL_QUIT:
+					done = 1;
+					break;
+			} //End switch-statements
+		} //End while event loop
+		
+		//RGB + ALPHA
+		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+		
+		SDL_RenderClear(renderer);
+		
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		
+		SDL_Rect rect = { 220, 140, 200, 200 };
+		SDL_RenderFillRect(renderer, &rect); 
+		
+		SDL_RenderPresent(renderer);
+		
+		//Delay 10 msec
+		SDL_Delay(10);
+	} //End while !done loop
+
 	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
 	//Quit SDL
 	SDL_Quit();
 	return 0;
