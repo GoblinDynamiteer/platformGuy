f811d6bb42542aa61558b76c412f0c036851ead3
Johan Kämpe <kampe.johan@gmail.com>
2017-01-04 19:02:07 +0100

    Added function for setting player status, bitwise

--- lib/status.c
+++ lib/status.c
@@ -1,8 +1,19 @@
 #include "..\def.h"
 
-bool getStatus(game * game, int check){
+//gets player status for CHECK
+bool getPlayerStatus(game * game, int check){
 	if(gPlayer.status & check){
 		return 1;
 	}
 	return 0;
+}
+
+//Sets/unsets player status
+void setPlayerStatus(game * game, int status, bool onOff){
+	if(onOff){
+		gPlayer.status |= status; //Bitwise OR
+	}
+	else{
+		gPlayer.status &= (~status); //Bitwise NOT
+	}
 }
