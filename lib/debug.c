0630e6c06b3a36f5248233b3aeaac15c83a108f7
Johan Kämpe <kampe.johan@gmail.com>
2017-01-05 22:20:13 +0100

    Added debug output

--- lib/debug.c
+++ lib/debug.c
@@ -2,6 +2,7 @@
 
 void debugInfo(game * game){
 	if(gTimer % DEBUG_OUTPUT_SPEED == 0){
+		int playerFeet = gPlayer Y + gPlayer.hitbox.h;
 		if(DEBUG_MOVEMENT){
 			if(getPlayerStatus(game, STATUS_RUNNING)){
 				printf("[ Running ");
@@ -14,16 +15,23 @@ void debugInfo(game * game){
 				printf(" [Velocity L/R: %g/%g ] [Player X: %d ]\n", 
 					gPlayer.velocity.left, gPlayer.velocity.right, gPlayer X);
 			}
+			if(getPlayerStatus(game, STATUS_SKIDDING)){
+				printf(" [ Skidding! ] \n");
+			}
 		}
 		if(DEBUG_JUMPING){
 			if(getPlayerStatus(game, STATUS_AIRBORNE)){
-				printf("[ Jumping ] [Velocity: %g ] [Player Y: %d ]\n", 
+				printf("[ Jumping ] [Velocity: %g ] [Player Y: %d ] ", 
 					gPlayer.velocity.up, gPlayer Y);
 			}
+			printf("[ Feet Y: %d ] \n", playerFeet);
 		}
 		if(DEBUG_HITBOX){
 			printf("[ Player Hitbox H/W: %d/%d ]\n",
 				gPlayer.hitbox.h, gPlayer.hitbox.w);
+		}
+		if(playerFeet>LANDLINE){
+		printf("[ Under Landline! ] \n");
 		}
 	}
 }
