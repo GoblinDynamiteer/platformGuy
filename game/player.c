c52c7bf78fd1826d9d7e46ea78a2e576a9565f76
Johan Kämpe <kampe.johan@gmail.com>
2017-01-09 22:35:48 +0100

    Added cap of max speed to player velocity function.

--- game/player.c
+++ game/player.c
@@ -10,9 +10,15 @@
 void playerSpeed(game * game, bool direction){
 	if(direction == LEFT){
 		game->player.speed.x -= RUN_ACC;
+		if(fabs(game->player.speed.x) >= MAX_SPEED){
+			game->player.speed.x = MAX_SPEED * -1;
+		}
 	}
 	else{
 		game->player.speed.x += RUN_ACC;
+		if(fabs(game->player.speed.x) >= MAX_SPEED){
+			game->player.speed.x = MAX_SPEED;
+		}
 	}
 }
 
