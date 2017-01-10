d9269aaebd590344b4995a49c69ad8b836514391
Johan Kämpe <kampe.johan@gmail.com>
2017-01-10 22:38:28 +0100

    Style changes.

--- main.c
+++ main.c
@@ -1,31 +1,25 @@
 #include "def.h"
 
-int main(int argc, char* args[]){
+int main(int argc, char* args[]) {
 
 	/*	 Start SDL Systems	*/
 	SDL_Init(SDL_INIT_EVERYTHING);
-    IMG_Init(IMG_INIT_PNG);
+	IMG_Init(IMG_INIT_PNG);
 
 	/*	 Declare window and renderer	*/
 	SDL_Window *window;
 	SDL_Renderer *renderer;
-	
-    /*	 Create window	*/
-	window = SDL_CreateWindow(
-		"PlatformGuy",
-		SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,
-		WINDOW_WIDTH,
-		WINDOW_HEIGHT,
-		0
-	);
+
+	/*	 Create window	*/
+	window = SDL_CreateWindow("PlatformGuy",
+	SDL_WINDOWPOS_UNDEFINED,
+	SDL_WINDOWPOS_UNDEFINED,
+	WINDOW_WIDTH,
+	WINDOW_HEIGHT, 0);
 
 	/*	 Create renderer	*/
-	renderer = SDL_CreateRenderer(
-		window,
-		-1,
-		RENDERER_FLAGS
-	);
+	renderer = SDL_CreateRenderer(window, -1,
+	RENDERER_FLAGS);
 
 	/*	 Game struct	*/
 	game game;
@@ -33,9 +27,9 @@ int main(int argc, char* args[]){
 	game.window = window;
 
 	/*	 Load art, set starting status	*/
-	if(loadGame(&game) && loadTextures(&game)){
+	if (loadGame(&game) && loadTextures(&game)) {
 		/*	 Game loop	*/
-		while(getEvents(&game)){
+		while (getEvents(&game)) {
 			renderGame(&game);
 			/*	 60 fps	*/
 			SDL_Delay(1000 / 60);
