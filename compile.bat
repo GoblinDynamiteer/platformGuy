c9ab793ee4e1078c7ea54398a19b0c6fca735fc8
Johan Kämpe <kampe.johan@gmail.com>
2017-01-10 21:51:33 +0100

    Updated compile batch file.

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-gcc main.c game\draw.c game\load.c game\event.c game\player.c -o platform -lmingw32 -lSDL2main -lSDL2 -lSDL2_image 
+gcc main.c game\draw.c game\load.c game\event.c game\player.c game\world.c -o platform -lmingw32 -lSDL2main -lSDL2 -lSDL2_image 
