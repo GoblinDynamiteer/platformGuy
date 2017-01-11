d310123ebef6404506bc114933342a385a032a13
Johan Kämpe <kampe.johan@gmail.com>
2017-01-11 15:03:22 +0100

    Added more keyboard events

--- game/event.c
+++ game/event.c
@@ -24,25 +24,45 @@ bool getEvents(game * game){
 						/*	 Escape button	*/
 						case SDLK_ESCAPE:
 							return 0;
+						/*	 Up arrow key, jumps	*/
 						case SDLK_UP:
 							playerJump(game);
 							break;
+						/*	 Down arrow key, ducks	*/
+						case SDLK_DOWN:
+							game->player.status[DUCK] = TRUE;
+							break;
 					}
 					break;
+				/*	 A key has been released	*/
+				case SDL_KEYUP:
+					switch(event.key.keysym.sym){
+						case SDLK_DOWN:
+							game->player.status[DUCK] = FALSE;
+							break;
+						case SDLK_LEFT:
+						case SDLK_RIGHT:
+							if(fabs(game->player.speed.x) > MAX_SPEED - 1){
+								game->player.status[SKID] = TRUE;
+							}
+							break;
+					}
 			}
 		}
 
 	/*	 Gets key states	*/
 	const Uint8 *state = SDL_GetKeyboardState(NULL);
 
-	if(state[SDL_SCANCODE_RIGHT]){
-		playerSetSpeed(game, RIGHT);
-		game->player.direction = RIGHT;
-	}
+	if(!game->player.status[DUCK]){
+		if(state[SDL_SCANCODE_RIGHT]){
+			playerSetSpeed(game, RIGHT);
+			game->player.direction = RIGHT;
+		}
 
-	if(state[SDL_SCANCODE_LEFT]){
-		playerSetSpeed(game, LEFT);
-		game->player.direction = LEFT;
+		if(state[SDL_SCANCODE_LEFT]){
+			playerSetSpeed(game, LEFT);
+			game->player.direction = LEFT;
+		}
 	}
 
 	if(state[SDL_SCANCODE_UP] &&
